e9bb67a5fa8f864202e2166bd96a6157ac58bafa
jamesbindin <jamesbindin@googlemail.com>
2026-02-21 15:20:42 +0000

    edits to migrations. adding address. Re adding user sessions

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name', 50);
+            $table->string('email', 50)->unique();
+            $table->string('address', 200);
+            // $table->timestamp('email_verified_at')->nullable();
+            $table->string('password', 200);
+            // $table->rememberToken();
             $table->timestamps();
         });
 
@@ -43,7 +44,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
+        // Schema::dropIfExists('password_reset_tokens');
+        // Schema::dropIfExists('sessions');
     }
 };
