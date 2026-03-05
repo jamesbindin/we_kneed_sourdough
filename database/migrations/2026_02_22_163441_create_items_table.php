d701c148acd4c1b563586879c2fa0dfce6dee38d
jamesbindin <jamesbindin@googlemail.com>
2026-03-05 13:12:51 +0000

    moved items population into its own migration file

--- database/migrations/2026_02_22_163441_create_items_table.php
+++ database/migrations/2026_02_22_163441_create_items_table.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -21,21 +20,6 @@ return new class extends Migration
             $table->timestamps();
         });
 
-        DB::table('items')->insert(
-            array(
-                'name' => 'margherita', 'description' => 'Classic margherita pizza with a sourdough crust. Authentic tomato sauce topped with mozzarella cheese.', 'price' => 10.99, 'image' => 'margherita.jpg',
-            )
-        );
-        DB::table('items')->insert(
-            array(
-                'name' => 'neapolitan', 'description' => 'Authentic Neapolitan with a sourdough base. Crispy thin crust topped with home made tomato sauce with fresh mozzarella and basil.', 'price' => 10.99, 'image' => 'neapolitan.jpg',
-            )
-        );
-        DB::table('items')->insert(
-            array(
-                'name' => 'chicken fajita', 'description' => 'Spicy fajita pizza on a sourdough base. Tomato base with mozzarella cheese. Topped with charred onion, bell pepper chicken and fresh chilli spiced with cumin. ', 'price' => 12.99, 'image' => 'fajita.jpg',
-            )
-        );
     }
 
     /**
